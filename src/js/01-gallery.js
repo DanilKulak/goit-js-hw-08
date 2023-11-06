bb26ca0257b5666d8361fd070c4bf6051ad47d9d
DanilKulak <1998kylak@gmail.com>
2023-11-06 19:05:10 +0200

    task-01-03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach((item) => {
+  const galleryItemHTML = `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+      </a>
+    </li>
+  `;
+  gallery.insertAdjacentHTML('beforeend', galleryItemHTML);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+});
